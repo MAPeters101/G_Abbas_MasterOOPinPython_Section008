126d7f51a4c80bf767d5e7ba2e73165ff7cc3af1
Mark Peters <mapeters101@earthlink.net>
2025-05-26 17:22:42 -0400

    Work on lesson017 to 3:50.

---
+++ lesson017.py
@@ -0,0 +1,13 @@
+class Pakistan:
+    def language(self):
+        print("Urdu")
+
+class Punjab(Pakistan):
+    def language(self):
+        print("Punjabi")
+
+class KPK(Pakistan):
+    def language(self):
+        print("Pasto")
+
+
