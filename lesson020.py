8273f3026b0c4a2d2e315987554a8f236f2a5697
Mark Peters <mapeters101@earthlink.net>
2025-05-26 17:51:53 -0400

    Complete work on lesson020 from 3:40.

--- lesson020.py
+++ lesson020.py
@@ -3,8 +3,17 @@ class Calculator:
         self.num1 = num1
         self.num2 = num2
 
+    def __add__(self, other):
+        return self.num1 + other.num1, self.num2 + other.num2
+
+    def __sub__(self, other):
+        return self.num1 - other.num1, self.num2 - other.num2
+
 obj1 = Calculator(3,7)
 obj2 = Calculator(4,2)
 
 obj3 = obj1 + obj2
 print(obj3)
+
+obj4 = obj1 - obj2
+print(obj4)
