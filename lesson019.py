1ac0ae5c347b4942c7280ca4b21e86bd328211d2
Mark Peters <mapeters101@earthlink.net>
2025-05-26 17:41:49 -0400

    Work on lesson019 to 5:15.

---
+++ lesson019.py
@@ -0,0 +1,25 @@
+class Account:
+    def createAccount(self, name, email, password, mobileno):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.mobileno = mobileno
+
+    def createAccount(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def showData(self):
+        print(self.name)
+        print(self.email)
+        print(self.password)
+        print(self.mobileno)
+
+
+acc = Account()
+acc.createAccount("John", "john@gmail.com", "password", 123456789)
+acc.showData()
+
+
+
